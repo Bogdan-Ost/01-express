936e33e3a910708c6130476a3d60ed1dd2035643
Ostapenko Bogdan <bogdan.ostapenko.97@gmail.com>
2026-03-17 19:43:36 +0200

    finaly

--- src/controllers/notesControllers.js
+++ src/controllers/notesControllers.js
@@ -16,3 +16,35 @@ export const getNoteById = async (req, res) => {
 
   res.status(200).json(note);
 };
+
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+  res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
+    returnDocument: 'after',
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,9 +1,17 @@
-import { getAllNotes, getNoteById } from '../controllers/notesControllers';
+import {
+  createNote,
+  deleteNote,
+  getAllNotes,
+  getNoteById,
+} from '../controllers/notesControllers';
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
+router.post('/notes', createNote);
+router.delete('/notes/:noteId', deleteNote);
+router.patch('/notes/:noteId', createNote);
 
 export default router;
